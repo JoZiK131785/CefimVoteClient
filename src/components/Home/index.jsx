529f6d66e6d67503fdc5a48c849105a4a2390cc0
unknown <jordane.zicry@gmail;com>
2023-05-02 15:36:27 +0200

    Version 0.1 : ajout client & WebSocket

---
+++ src/components/Home/index.jsx
@@ -0,0 +1,30 @@
+import './index.css';
+import React, { useState, useEffect } from 'react';
+
+const Home = ({ socket }) => {
+
+    const [user, setUser] = useState("");
+
+    useEffect(() => {
+        socket.on('newUserResponse', (data) => console.log(data));
+    }, [socket]);
+
+    function test()
+    {
+        // Ajoute le joueur a la liste des joueurs connectes
+        socket.emit("newUser", { user, socketID: socket.id });
+    }
+
+    return (
+        <div className="home">
+            <input 
+            type="text"
+            value={user}
+            onChange={(e) => setUser(e.target.value)}
+            />
+            <button onClick={test}></button>
+        </div>
+    );
+}
+
+export default Home;
