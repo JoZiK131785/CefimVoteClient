391f41d78345c1548af81e756297d6028e634faf
Jordane Zicry <jordanezicry@gmail.com>
2023-05-04 15:18:23 +0200

    MEP logs&ws

--- src/components/FormLog/index.jsx
+++ src/components/FormLog/index.jsx
@@ -1,21 +1,35 @@
 import "./index.css";
 import MainButton from "../MainButton";
+import { useRef } from "react";
 
-const FormLog = ({ title, labelOne, labelTwo, placeholderOne, placeholderTwo, type, value }) => {
+const FormLog = ({ title, labelOne, labelTwo, placeholderOne, placeholderTwo, type, value, submitFunction, index }) => {
+
+    const firstInput = useRef(null);
+    const secondInput = useRef(null);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const firstString = firstInput.current.value;
+        const secondString = secondInput.current.value;
+
+        submitFunction(firstString, secondString, index);
+    };
+
     return (
-        <form action="" className="form-log">
+        <form action="submit" className="form-log" onSubmit={handleSubmit}>
             <h2 className="title-form">
                 {title}
             </h2>
 
             <div className="form-input first-input">
-                <label for="name">{labelOne}</label>
-                <input type="text" name="name" id="input-name" placeholder={placeholderOne} />
+                <label htmlFor="name">{labelOne}</label>
+                <input type="text" name="name" placeholder={placeholderOne} ref={firstInput} />
             </div>
 
             <div className="form-input secondary-input">
-                <label for="name">{labelTwo}</label>
-                <input type={type} name="name" id="input-name" placeholder={placeholderTwo} />
+                <label htmlFor="name">{labelTwo}</label>
+                <input type={type} name="name" placeholder={placeholderTwo} ref={secondInput} />
             </div>
 
             <div className="form-button">
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,32 +1,30 @@
 import Header from '../Header';
 import FormLog from '../FormLog';
 import './index.css';
-import React, { useState, useEffect } from 'react';
 
 const Home = ({ socket }) => {
 
-    const [user, setUser] = useState("");
+    const tryLogin = (firstInput, secondInput, index) => {
+        index === 1 ? adminLogin(firstInput, secondInput) : userLogin(firstInput, secondInput);
+    }
 
-    useEffect(() => {
-        socket.on('newUserResponse', (data) => console.log(data));
-    }, [socket]);
+    const adminLogin = (userName, password) => {
+        if(userName === "admin" && password === "admin") {
+            console.log("admin");
+        }
+        else
+            console.log("not admin");
+    }
 
-    function test() {
-        // Ajoute le joueur a la liste des joueurs connectes
-        socket.emit("newUser", { user, socketID: socket.id });
+    const userLogin = (userName, code) => {
+        if(code === "0000") {
+            socket.emit("newUser", { userName, socketID: socket.id });
+        }
+        else
+            console.log("not user");
     }
 
     return (
-        // <div className="home">
-        //     <input 
-        //     type="text"
-        //     value={user}
-        //     onChange={(e) => setUser(e.target.value)}
-        //     />
-        //     <button onClick={test}></button>
-        // </div>
-
-
         <>
             <Header />
 
@@ -35,12 +33,15 @@ const Home = ({ socket }) => {
                 <h1 className="title-page">Connexion</h1>
 
                 <div className="container-forms">
+
                     <FormLog
                         title="En tant qu'administrateur.ice :" labelOne="Nom d'administrateur.ice :" labelTwo="Mot de passe :"
                         placeholderOne="Carmen Blabla"
                         placeholderTwo="...."
                         type="password"
                         value="Se connecter"
+                        submitFunction={tryLogin}
+                        index={1}
                     />
 
                     <FormLog
@@ -49,7 +50,10 @@ const Home = ({ socket }) => {
                         placeholderTwo="...."
                         type="text"
                         value="Se connecter"
+                        submitFunction={tryLogin}
+                        index={2}
                     />
+                    
                 </div>
             </section>
 
